35a8574981f3b0080ab4e1e0e1f1aaf1382ef9af
Shree Dittakavi <shreekavi@gmail.com>
2010-09-11 01:43:54 +0530

    Plain Rails3 commit"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muzum::Application.config.secret_token = '878ce5d5cca7f02ab8e0d5896031e60f4ea5eb5d3735f621ffddfd1ab24b3538a91052d0141b08a91d112a4fcece1ed477cfa0c0d62e4b9cae471b8ce3dca29d'
